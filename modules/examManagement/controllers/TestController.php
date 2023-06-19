60ea1ca1485ad8a76fe504fa71001a9ee96c057d
rufusy <idachirufus@gmail.com>
2023-06-19 10:17:29 +0300

    Test controller on test branch

---
+++ modules/examManagement/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Rufusy Idachi <idachirufus@gmail.com>
+ * @date: 6/19/2023
+ * @time: 10:14 AM
+ */
+
+namespace app\modules\examManagement\controllers;
+
+class TestController extends BaseController
+{
+    public function actionIndex()
+    {
+        echo "Test controller";
+    }
+}
